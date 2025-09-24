788445fa36d074fee4d0e99297790f3e2cbae6e1
KaweVK <victorkawe07@gmail.com>
2025-09-24 20:10:54 -0300

    Adicionando gráfio modelo geo

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit as st
-import plotly as plt
+import plotly.express as px
 import requests as req 
 
 def formatar_valor(valor, prefixo = ""):
@@ -15,9 +15,26 @@ url = "https://labdados.com/produtos"
 response = req.get(url)
 dados = pd.DataFrame.from_dict(response.json())
 
+## Tabelas 
+receita_estados = dados.groupby('Local da compra')[['Preço']].sum()
+receita_estados = dados.drop_duplicates(subset='Local da compra')[['Local da compra', 'lat', 'lon']].merge(receita_estados, left_on='Local da compra', right_index=True).sort_values('Preço', ascending=False)
+
+# Graficos
+fig_mapa_receita = px.scatter_geo(receita_estados,
+                                   lat = 'lat',
+                                   lon = 'lon',
+                                   scope='south america',
+                                   size = 'Preço',
+                                   template = 'seaborn',
+                                   hover_name = 'Local da compra',
+                                   hover_data = {'lat':False, 'lon':False},
+                                   title = 'Receita por Estado',)
+
+## Visualizações
 col1, col2 = st.columns(2)
 with col1:
   st.metric("Receita das Vendas", formatar_valor(dados['Preço'].sum(), "R$"), border= True) 
+  st.plotly_chart(fig_mapa_receita)
 with col2: 
    st.metric("Quantidade de Vendas", formatar_valor(dados.shape[0]))
 
