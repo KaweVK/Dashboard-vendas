524b83c80290195b28a1ec09de165b389cf272fc
KaweVK <victorkawe07@gmail.com>
2025-09-24 19:16:30 -0300

    Adicionando função para formatar números

--- dashboard.py
+++ dashboard.py
@@ -3,6 +3,12 @@ import streamlit as st
 import plotly as plt
 import requests as req 
 
+def formatar_valor(valor, prefixo = ""):
+   for unidade in ['', 'mil', 'milhões']:
+        if valor < 1000:
+            return f"{prefixo}{valor:.2f} {unidade}"
+        valor /= 1000
+
 st.title("Sales Dashboard :shopping_cart:")
 
 url = "https://labdados.com/produtos"
@@ -11,8 +17,8 @@ dados = pd.DataFrame.from_dict(response.json())
 
 col1, col2 = st.columns(2)
 with col1:
-  st.metric("Soma das Vendas", dados['Preço'].sum(), "R$", border= True) 
+  st.metric("Soma das Vendas", formatar_valor(dados['Preço'].sum(), "R$"), border= True) 
 with col2: 
-   st.metric("Quantidade de Vendas", dados.shape[0])
+   st.metric("Quantidade de Vendas", formatar_valor(dados.shape[0]))
 
 st.dataframe(dados)
