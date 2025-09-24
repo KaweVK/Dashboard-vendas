116172ad104db998f2dacb4acb80aacbc4b0c14c
KaweVK <victorkawe07@gmail.com>
2025-09-24 19:15:54 -0300

    Adicionando métricas

--- dashboard.py
+++ dashboard.py
@@ -9,4 +9,10 @@ url = "https://labdados.com/produtos"
 response = req.get(url)
 dados = pd.DataFrame.from_dict(response.json())
 
+col1, col2 = st.columns(2)
+with col1:
+  st.metric("Soma das Vendas", dados['Preço'].sum(), "R$", border= True) 
+with col2: 
+   st.metric("Quantidade de Vendas", dados.shape[0])
+
 st.dataframe(dados)
