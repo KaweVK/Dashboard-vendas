38c1e5e5f2457ee2c6d305565dd6fcf8d820ce94
KaweVK <victorkawe07@gmail.com>
2025-10-14 15:57:12 -0300

    Colocando novos filtros e page config

--- pages/Dados brutos.py
+++ pages/Dados brutos.py
@@ -3,6 +3,7 @@ import pandas as pd
 import requests as req
 
 st.title('Dados brutos')
+st.set_page_config(page_title="Dados Burtos", page_icon=":bar_chart:", layout="wide")
 
 url = 'https://labdados.com/produtos'
 
@@ -13,15 +14,26 @@ dados['Data da Compra'] = pd.to_datetime(dados['Data da Compra'], format = '%d/%
 aba1 = st.tabs(['Dados'])
 
 st.sidebar.title('Filtros')
-with st.sidebar.expander('Nome do Produto'):
+with st.sidebar.expander('Nome do produto'):
     produtos = st.multiselect('Selecione os produtos', dados['Produto'].unique(), dados['Produto'].unique())
-with st.sidebar.expander('Preço do Produto'):
-    preco = st.slider('Selecione o preço', 0, 5000, (0, 5000))
-with st.sidebar.expander('Data da Compra'):
+with st.sidebar.expander('Categoria do produto'):
+    categoria = st.multiselect('Selecione as categorias', dados['Categoria do Produto'].unique(), dados['Categoria do Produto'].unique())
+with st.sidebar.expander('Preço do produto'):
+    preco = st.slider('Selecione o preço', 0, 5000, (0,5000))
+with st.sidebar.expander('Frete da venda'):
+    frete = st.slider('Frete', 0,250, (0,250))
+with st.sidebar.expander('Data da compra'):
     data_compra = st.date_input('Selecione a data', (dados['Data da Compra'].min(), dados['Data da Compra'].max()))
-
-with st.expander('Colunas'):
-    colunas = st.multiselect('Selecione as colunas', list(dados.columns), list(dados.columns))
+with st.sidebar.expander('Vendedor'):
+    vendedores = st.multiselect('Selecione os vendedores', dados['Vendedor'].unique(), dados['Vendedor'].unique())
+with st.sidebar.expander('Local da compra'):
+    local_compra = st.multiselect('Selecione o local da compra', dados['Local da compra'].unique(), dados['Local da compra'].unique())
+with st.sidebar.expander('Avaliação da compra'):
+    avaliacao = st.slider('Selecione a avaliação da compra',1,5, value = (1,5))
+with st.sidebar.expander('Tipo de pagamento'):
+    tipo_pagamento = st.multiselect('Selecione o tipo de pagamento',dados['Tipo de pagamento'].unique(), dados['Tipo de pagamento'].unique())
+with st.sidebar.expander('Quantidade de parcelas'):
+    qtd_parcelas = st.slider('Selecione a quantidade de parcelas', 1, 24, (1,24))
 
 st.dataframe(dados)
 
