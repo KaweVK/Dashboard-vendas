965b9383a750437e549c46cc8c7bc974635bea2a
KaweVK <victorkawe07@gmail.com>
2025-10-14 16:26:03 -0300

    Novos filtros

--- pages/Dados brutos.py
+++ pages/Dados brutos.py
@@ -40,8 +40,15 @@ with st.sidebar.expander('Quantidade de parcelas'):
 
 query = '''
 Produto in @produtos and \
+`Categoria do Produto` in @categoria and \
 @preco[0] <= Preço <= @preco[1] and \
-@data_compra[0] <= `Data da Compra` <= @data_compra[1]
+Frete >= @frete[0] and Frete <= @frete[1] and \
+@data_compra[0] <= `Data da Compra` <= @data_compra[1] and \
+Vendedor in @vendedores and \
+`Local da compra` in @local_compra and \
+@avaliacao[0] <= `Avaliação da compra` <= @avaliacao[1] and \
+`Tipo de pagamento` in @tipo_pagamento and \
+@qtd_parcelas[0] <= `Quantidade de parcelas` <= @qtd_parcelas[1]
 '''
 
 dados_filtrados = dados.query(query)
